a935939496f59812ecffc1c1fa5a640ef6bd1f44
Vena Ketpongsuda <>
2019-12-18 18:47:24 -0800

    WorldTakeOver: Collisions

--- WorldTakeOver/Assets/Scripts/MusicPlayer.cs
+++ WorldTakeOver/Assets/Scripts/MusicPlayer.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    [SerializeField] float LevelDelay = 3f;
 
     private void Awake()
     {
@@ -15,12 +13,7 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("loadNextScene", LevelDelay);
-    }
-
-    public void loadNextScene()
-    {
-        SceneManager.LoadScene(1);
+       
     }
 
 }
---
+++ WorldTakeOver/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float LevelDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("loadNextScene", LevelDelay);
+    }
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
